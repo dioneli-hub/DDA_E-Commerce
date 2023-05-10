5084645530c04644bfcf461b94f0f177e3ab39dd
Dastan Abishev <abish.ev@mail.ru>
2023-05-10 19:49:17 +0200

    Initial

---
+++ DDA.DataAccess/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DDA.DataAccess
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ DDA.DataAccess/Entities/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDA.DataAccess.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
