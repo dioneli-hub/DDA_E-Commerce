33104e16cb4c6b59733d0e27abef82268b40953e
Diana Levchenko <dianka_levchenko@inbox.ru>
2023-06-06 23:16:25 +0200

    Registration page added.

--- DDA.BusinessLogic/Services/UserService/IUserService.cs
+++ DDA.BusinessLogic/Services/UserService/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DDA.ApiModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace DDA.BusinessLogic.Services.UserService
     public interface IUserService
     {
         public int GetCurrentUserId();
+        Task<UserModel> Register(RegisterUserModel registerUserModel);
     }
 }
--- DDA.BusinessLogic/Services/UserService/UserService.cs
+++ DDA.BusinessLogic/Services/UserService/UserService.cs
@@ -1,9 +1,20 @@
-﻿using DDA.BusinessLogic.UserContext;
+﻿using DDA.ApiModels;
+using DDA.BusinessLogic.UserContext;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace DDA.BusinessLogic.Services.UserService
 {
     public class UserService : IUserService
     {
+
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         //private readonly IUserContextService _userContextService;
         public UserService()//IUserContextService userContextService
         {
@@ -13,5 +24,34 @@ namespace DDA.BusinessLogic.Services.UserService
         {
             return 1;//_userContextService.GetCurrentUserId();
         }
+
+        public async Task<UserModel> Register(RegisterUserModel registerUserModel)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync<RegisterUserModel>("api/User", registerUserModel);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode is System.Net.HttpStatusCode.NoContent)
+                    {
+                        return default(UserModel);
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<UserModel>();
+                }
+
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Http status:{response.StatusCode} Message - {message}");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
     }
 }
